acbc2480b360177d2d1fbefe44e0278f187fbff6
DichMS <86953785+DichMS@users.noreply.github.com>
2021-07-05 12:35:05 +0300

    Дать нормальное имя

--- Group_1/Form1.Designer.cs
+++ Group_1/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace Group_1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Formochka";
+            this.Text = "API VK";
             this.ResumeLayout(false);
 
         }
